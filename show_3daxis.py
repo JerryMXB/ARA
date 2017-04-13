9951c5108ca6d6d2a4d459cfb56cfc7c15c2d0b9
pppplin <wz723@nyu.edu>
2017-04-13 15:48:55 -0400

    small fix

--- show_3daxis.py
+++ show_3daxis.py
@@ -1,41 +1,41 @@
 from webcam import Webcam
 import numpy as np
 import cv2
-from datetime import datetime
+
+print(cv2.__version__)
+
+NROW = 6
+NCOL = 4
 
 
 def draw(img, corners, imgpts):
     corner = tuple(corners[0].ravel())
-    cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
-    cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
-    cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+
+    cv2.line(img, corner, tuple(imgpts[0].ravel()), (255, 0, 0), 5)
+    cv2.line(img, corner, tuple(imgpts[1].ravel()), (0, 255, 0), 5)
+    cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
     return img
 
 
 def drawcube(img, corners, imgpts):
-    imgpts = np.int32(imgpts).reshape(-1,2)
-
+    imgpts = np.int32(imgpts).reshape(-1, 2)
     # draw ground floor in green
-    cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
-
-    # draw pillars in blue color
-    for i,j in zip(range(4),range(4,8)):
-        cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
-
-    # draw top layer in red color
-    cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
-
+    cv2.drawContours(img, [imgpts[:4]], -1, (0, 255, 0), -3)
+    # draw pillars in blue
+    for i, j in zip(range(4), range(4, 8)):
+        cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]), (255), 3)
+    # draw top layer in red
+    cv2.drawContours(img, [imgpts[4:]], -1, (0, 0, 255), 3)
     return img
 
-
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-#axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0], #cube
-                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ])
-with np.load('pose\\webcam_calibration_ouput.npz') as X:
-    mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+objp = np.zeros((NROW * NCOL, 3), np.float32)
+objp[:, :2] = np.mgrid[0:NROW, 0:NCOL].T.reshape(-1, 2)
+axis = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0],  #cube
+[0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
+
+with np.load('pose/webcam_calibration_ouput.npz') as X:
+    mtx, dist, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
 webcam = Webcam()
 webcam.start()
@@ -44,24 +44,27 @@ while True:
     # get image from webcam
     img = webcam.get_current_frame()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+    # adapted to inner corners
+    ret, corners = cv2.findChessboardCorners(gray, (NROW, NCOL), None)
 
     if ret == True:
         print('I find you')
         cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         # Find the rotation and translation vectors.
-        rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners, mtx, dist)
+        try:
+            rvecs, tvecs, _ = cv2.solvePnPRansac(objp, corners, mtx, dist)
+        except:
+            _, rvecs, tvecs = cv2.solvePnP(objp, corners, mtx, dist)
 
         # project 3D points to image plane
         imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
-        #print(imgpts)
         img = drawcube(img, corners, imgpts)
-        #cv2.drawChessboardCorners(img, (7, 6), corners, ret)
         if img is not None:
             cv2.imshow('img', img)
             cv2.waitKey(10)
         else:
-            print ('but I cannot display!')
+            print('but I cannot display!')
     else:
         cv2.imshow('img', img)
         cv2.waitKey(10)
+
--- webcam_calibration.py
+++ webcam_calibration.py
@@ -2,42 +2,42 @@ import numpy as np
 import cv2
 import glob
 
+NROW = 8
+NCOL = 6
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# eg: checkerboard: inner points
+objp = np.zeros((NCOL * NROW, 3), np.float32)
+objp[:, :2] = np.mgrid[0:NROW, 0:NCOL].T.reshape(-1, 2)
+
 
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-print ('start')
-images = glob.glob('pose\\sample_images\\*.jpg')
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+print('start')
+# images = glob.glob('pose\\sample_images\\*.jpg')
+images = glob.glob('pose/sample_images_3/*.jpg')
 for fname in images:
-    print (fname)
+    print(fname)
     img = cv2.imread(fname)
     if img is None:
         print('It is empty')
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (NROW, NCOL), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints.append(objp)
-
-        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
         imgpoints.append(corners)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners,ret)
+        cv2.drawChessboardCorners(img, (NROW, NCOL), corners, ret)
         cv2.imshow('image', img)
-        cv2.waitKey(2000)
+        cv2.waitKey(5)
 
 # calibrate webcam and save output
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-np.savez("pose/webcam_calibration_ouput", ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
+np.savez("pose/webcam_calibration_ouput_3", ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
 
 cv2.destroyAllWindows()
